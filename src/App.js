6ba1c55fba11504a48b8c6cc89f0044281cb201d
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-06-01 20:35:48 -0400

    Migrating Skeleton
    Moved the Skeleton out of the current Util and back into the App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,23 @@ import { user$, funky } from "./lib/API.js";
 import { useStore } from '@nanostores/react';
 
 
+const Skeleton = () => {
+  return (
+    <>
+      <div className="flex flex-col rounded shadow-md w-full animate-pulse h-128">
+        <div className="h-48 rounded-t bg-gray-700"></div>
+        
+        <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 bg-gray-900">
+          <div className="w-full h-6 rounded bg-gray-700"></div>
+          <div className="w-full h-6 rounded bg-gray-700"></div>
+          <div className="w-3/4 h-6 rounded bg-gray-700"></div>
+        </div>
+      </div>
+   </>
+  );
+};
+
+
 function App({ kbve }) {
   //const _limit = kbve.getAttribute("data-limit");
   //const _dataJSON = kbve.getAttribute("data-json");
@@ -27,7 +44,7 @@ function App({ kbve }) {
 
   return (
     <>
-      {loading && Util.skeleton()}
+      {loading && Skeleton()}
       {!loading && <Conch />}
       
       
--- src/lib/Util.js
+++ src/lib/Util.js
@@ -1,24 +1,7 @@
 //?         Utility Class
-import React, { useEffect, useState } from "react";
-
 let Util = {
 
   sleep: (ms) => new Promise((resolve) => setTimeout(resolve, ms)),
-  skeleton: () => {
-    return (
-      <>
-        <div className="flex flex-col rounded shadow-md w-full animate-pulse h-128">
-          <div className="h-48 rounded-t bg-gray-700"></div>
-          
-          <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 bg-gray-900">
-            <div className="w-full h-6 rounded bg-gray-700"></div>
-            <div className="w-full h-6 rounded bg-gray-700"></div>
-            <div className="w-3/4 h-6 rounded bg-gray-700"></div>
-          </div>
-        </div>
-     </>
-    );
-  },
-};
+}
 
 export default Util;
